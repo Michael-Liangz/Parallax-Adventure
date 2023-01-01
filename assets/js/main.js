c053a0eb0ef1a0411e41ec9de8c3d06de6a80bac
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-01 19:14:57 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,47 @@ window.addEventListener("scroll", function () {
   forest.style.top = value * 0.25 + "px";
   header.style.top = value * 0.5 + "px";
 });
+
+// TODO ======================= Gsap ScrollTrigger JS =======================
+gsap.from("#m1", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  y: 400,
+});
+gsap.from("#m2", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  y: 100,
+});
+gsap.from("#t2", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: -500,
+});
+gsap.from("#t1", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: 500,
+});
+gsap.from("#man", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: -250,
+});
+gsap.from("#plants", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: -500,
+});
+gsap.from(".text-mountain", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: 500,
+});
